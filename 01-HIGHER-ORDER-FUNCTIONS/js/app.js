55be09f921deeedfe62da47ae0ab18e575469a93
[Rodrigo] <[roglobar10@gmail.com]>
2020-04-28 20:54:15 +0200

    Ejercicio buscador autos

---
+++ 01-HIGHER-ORDER-FUNCTIONS/js/app.js
@@ -0,0 +1,166 @@
+const autos = [
+	{
+		marca: 'BMW',
+		modelo: 'Serie 3',
+		year: 2012,
+		precio: 30000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ marca: 'Audi', modelo: 'A4', year: 2018, precio: 40000, puertas: 4, color: 'Negro', transmision: 'automatico' },
+	{
+		marca: 'Ford',
+		modelo: 'Mustang',
+		year: 2015,
+		precio: 20000,
+		puertas: 2,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ marca: 'Audi', modelo: 'A6', year: 2010, precio: 35000, puertas: 4, color: 'Negro', transmision: 'automatico' },
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2016,
+		precio: 70000,
+		puertas: 4,
+		color: 'Rojo',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2015,
+		precio: 25000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Chevrolet',
+		modelo: 'Camaro',
+		year: 2018,
+		precio: 60000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
+	{ marca: 'Ford', modelo: 'Mustang', year: 2019, precio: 80000, puertas: 2, color: 'Rojo', transmision: 'manual' },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2017,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ marca: 'Audi', modelo: 'A3', year: 2017, precio: 55000, puertas: 2, color: 'Negro', transmision: 'manual' },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2012,
+		precio: 25000,
+		puertas: 2,
+		color: 'Rojo',
+		transmision: 'manual'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2018,
+		precio: 45000,
+		puertas: 4,
+		color: 'Azul',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2019,
+		precio: 90000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ marca: 'Ford', modelo: 'Mustang', year: 2017, precio: 60000, puertas: 2, color: 'Negro', transmision: 'manual' },
+	{
+		marca: 'Dodge',
+		modelo: 'Challenger',
+		year: 2015,
+		precio: 35000,
+		puertas: 2,
+		color: 'Azul',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 3',
+		year: 2018,
+		precio: 50000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'BMW',
+		modelo: 'Serie 5',
+		year: 2017,
+		precio: 80000,
+		puertas: 4,
+		color: 'Negro',
+		transmision: 'automatico'
+	},
+	{
+		marca: 'Mercedes Benz',
+		modelo: 'Clase C',
+		year: 2018,
+		precio: 40000,
+		puertas: 4,
+		color: 'Blanco',
+		transmision: 'automatico'
+	},
+	{ marca: 'Audi', modelo: 'A4', year: 2016, precio: 30000, puertas: 4, color: 'Azul', transmision: 'automatico' }
+];
+
+// for (let i = 0; i < autos.length; i++) {
+// 	console.table(autos[i]);
+	
+// }
+
+// forEach se ejecuta 1 vez por cada elemento
+// let resultado = [];
+// autos.forEach((auto) => {
+// 	if (auto.color === 'Rojo') {
+// 		resultado.push(auto);
+// 	}
+// });
+
+
+// map igual que un foreach pero crea un nuevo arreglo
+// autos.map((auto) => {
+//  console.log(auto);
+// });
+
+
+
+// filter extraer valores 
+// let resultado = autos.filter(auto => {
+// 	return auto.transmision === 'manual' && auto.year >= 2015
+// });
+// console.log(resultado);
+
+// find se trae el primero que encuentre que cumpla el arreglo
+// let resultado = autos.find(auto => auto.marca === 'BMW' );
+
+// console.log(resultado);
+
+// reduce toma todos los valores y retorna un valor unico
+
+// let resultado = autos.reduce((total, auto)  => total + auto.precio, 0);
+//  
+
+// some
+
+
